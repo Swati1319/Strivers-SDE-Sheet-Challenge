c55c9cab2baf57aa7f3ab4c00acef74ab6c06687
Swati Sharma <79269517+Swati1319@users.noreply.github.com>
2023-06-06 23:40:53 +0530

    Create NextPermutation.java

---
+++ Arrays/NextPermutation.java
@@ -0,0 +1,47 @@
+import java.util.* ;
+import java.io.*; 
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Solution 
+{
+	public static ArrayList<Integer> nextPermutation(ArrayList<Integer> A)
+	{
+		int n = A.size(); // size of the array.
+
+        // Step 1: Find the break point:
+        int ind = -1; // break point
+        for (int i = n - 2; i >= 0; i--) {
+            if (A.get(i) < A.get(i + 1)) {
+                // index i is the break point
+                ind = i;
+                break;
+            }
+        }
+
+        // If break point does not exist:
+        if (ind == -1) {
+            // reverse the whole array:
+            Collections.reverse(A);
+            return A;
+        }
+
+        // Step 2: Find the next greater element
+        // and swap it with A.get(ind):
+
+        for (int i = n - 1; i > ind; i--) {
+            if (A.get(i) > A.get(ind)) {
+                int tmp = A.get(i);
+                A.set(i, A.get(ind));
+                A.set(ind, tmp);
+                break;
+            }
+        }
+
+        // Step 3: reverse the right half:
+        List<Integer> sublist = A.subList(ind + 1, n);
+        Collections.reverse(sublist);
+
+        return A;
+    }
+}
