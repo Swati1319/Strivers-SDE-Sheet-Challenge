817568589eacaffc17b53802cbf0069d9cb65581
Swati Sharma <79269517+Swati1319@users.noreply.github.com>
2023-06-06 23:35:57 +0530

    Update PascalsTriangle.java

--- Arrays/PascalsTriangle.java
+++ Arrays/PascalsTriangle.java
@@ -1,24 +1,22 @@
-class Solution {
-    public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
-        List<Integer> row, pre = null;
-        for(int i = 0 ; i < numRows ; ++i)
-        {
-            row = new ArrayList<Integer>();
-            for(int j = 0; j <= i ; ++j)
-            {
-                if(j == 0 || j == i)
-                {
-                    row.add(1);
-                }
-                else{
-                    row.add(pre.get(j-1) + pre.get(j));
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static ArrayList<ArrayList<Long>> printPascal(int n) {
+        ArrayList<ArrayList<Long>> res = new ArrayList<ArrayList<Long>>();
+        ArrayList<Long> row, pre = null;
+        for (int i = 0; i < n; ++i) {
+            row = new ArrayList<Long>();
+            for (int j = 0; j <= i; ++j) {
+                if (j == 0 || j == i) {
+                    row.add(1L);
+                } else {
+                    row.add(pre.get(j - 1) + pre.get(j));
                 }
             }
             pre = row;
             res.add(row);
         }
         return res;
-        
     }
 }
