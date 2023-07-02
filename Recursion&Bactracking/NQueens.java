2ecb2f30f3b0cbbe1f9d657d5e701bb9622b3a77
Swati Sharma <79269517+Swati1319@users.noreply.github.com>
2023-07-02 22:13:37 +0530

    Create NQueens.java

---
+++ Recursion&Bactracking/NQueens.java
@@ -0,0 +1,47 @@
+import java.util.*;
+public class Solution {
+    static HashMap<Integer,Integer> diag=new HashMap<>();
+    static HashMap<Integer,Integer> opp=new HashMap<>();
+    static HashMap<Integer,Integer> col=new HashMap<>();
+    static HashMap<Integer,Integer> row=new HashMap<>();
+    static int[][] queen;
+    static ArrayList<ArrayList<Integer>> arr=new ArrayList<>();
+    public static void helper(int n){
+        if(n==queen.length){
+            ArrayList<Integer> temp=new ArrayList<>();
+            for(int i=0;i<queen.length;i++){
+                for(int j=0;j<queen.length;j++){
+                    temp.add(queen[i][j]);
+                }
+            }
+            arr.add(temp);
+            return;
+        }
+        for(int i=0;i<queen.length;i++){
+            if(!diag.containsKey(i-n) && !opp.containsKey(i+n) && !col.containsKey(i) && !row.containsKey(n)){
+                diag.put(i-n,1);
+                 opp.put(i+n,1);
+                 col.put(i,1);
+                 row.put(n,1);
+                queen[n][i]=1;
+                helper(n+1);
+                 queen[n][i]=0;
+                diag.remove(i-n);
+                opp.remove(i+n);
+                col.remove(i);
+                row.remove(n);
+            }
+        }
+        
+    }
+    public static ArrayList<ArrayList<Integer>> solveNQueens(int n) {
+        queen=new int[n][n];
+        arr.clear();
+        diag.clear();
+        opp.clear();
+        col.clear();
+        row.clear();
+        helper(0);
+        return arr;
+    }
+}
