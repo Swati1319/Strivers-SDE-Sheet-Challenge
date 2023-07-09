007a4aa9635a9f56d8afe0c6f67caef3e163808a
Swati Sharma <79269517+Swati1319@users.noreply.github.com>
2023-07-09 12:24:28 +0530

    Create Bottom View Of Binary Tree.java

---
+++ BinaryTree/Bottom View Of Binary Tree.java
@@ -0,0 +1,66 @@
+import java.util.* ;
+import java.io.*; 
+/*********************************************
+
+    class BinaryTreeNode {
+        int val;
+        BinaryTreeNode left;
+        BinaryTreeNode right;
+
+        BinaryTreeNode(int val) {
+            this.val = val;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+*********************************************/
+
+import java.util.*;
+public class Solution {
+    public static ArrayList<Integer> bottomView(BinaryTreeNode root) {
+         // Write your code here.    
+          Queue<Pair>q=new ArrayDeque<>();
+        Map<Integer,Integer>map=new TreeMap<>();
+        
+        q.add(new Pair(0,root));
+        
+        while(!q.isEmpty())
+        {
+            Pair cur=q.poll();
+            
+            // if(!map.containsKey(cur.hd))
+            // {
+            //     map.put(cur.hd,cur.node.data);
+            // }
+            
+            map.put(cur.hd,cur.node.val);
+            
+            if(cur.node.left!=null)
+            {
+                q.add(new Pair(cur.hd-1,cur.node.left));
+            }
+             if(cur.node.right!=null)
+            {
+                q.add(new Pair(cur.hd+1,cur.node.right));
+            }
+        }
+        ArrayList<Integer>ans=new ArrayList<>();
+        
+        for(Map.Entry<Integer,Integer> entry:map.entrySet())
+        {
+            ans.add(entry.getValue());
+        }
+        return ans;
+    }
+    static class Pair
+    {
+        int hd;
+        BinaryTreeNode node;
+        public Pair(int hd,BinaryTreeNode node)
+        {
+            this.hd=hd;
+            this.node=node;
+        }
+    }     
+    }
